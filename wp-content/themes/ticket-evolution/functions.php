47995543906e5619456429eda65dce8ed828da12
[L]earned Media <dev@learnedmedia.com>
2017-03-15 16:32:29 -0400

    Installed and set up wp smush and w3 cache

--- wp-content/themes/ticket-evolution/functions.php
+++ wp-content/themes/ticket-evolution/functions.php
@@ -3,7 +3,8 @@
 add_action( 'wp_enqueue_scripts', 'enqueue_styles' );
 function enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
-    wp_enqueue_style( 'child-style', get_stylesheet_uri() );
+    //commented out to allow for w3 cache to minify all stylesheets
+    //wp_enqueue_style( 'child-style', get_stylesheet_uri() );
     wp_enqueue_script( 'custom-scripts', get_stylesheet_directory_uri() . '/js/scripts.js');
 }
 
